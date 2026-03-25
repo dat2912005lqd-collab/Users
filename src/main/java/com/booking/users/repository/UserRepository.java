33e55dc192191e0238ab34b5be458afa0af58184
dat2912005lqd-collab <dat2912005lqd@gmail.com>
2026-03-25 22:34:11 +0700

    Update UserRepository.java

--- src/main/java/com/booking/users/repository/UserRepository.java
+++ src/main/java/com/booking/users/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.booking.users.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.booking.users.entity.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findByEmailAndPassword(String email, String password);
