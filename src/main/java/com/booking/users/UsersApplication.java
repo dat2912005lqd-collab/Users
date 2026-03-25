29fe5b8cc45cdc4d00c758a1aeacb073136275b9
dat2912005lqd-collab <dat2912005lqd@gmail.com>
2026-03-25 22:35:44 +0700

    Update UsersApplication.java

--- src/main/java/com/booking/users/UsersApplication.java
+++ src/main/java/com/booking/users/UsersApplication.java
@@ -2,7 +2,10 @@ package com.booking.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@EnableJpaRepositories(basePackages = "com.booking.users.repository")
+@EntityScan(basePackages = "com.booking.users.model")
 @SpringBootApplication
 public class UsersApplication {
 
