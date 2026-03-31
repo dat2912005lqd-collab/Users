113abc4b0aff9923d733ec1ff274d3cd095e5920
dat2912005lqd-collab <dat2912005lqd@gmail.com>
2026-03-31 09:16:21 +0700

    Remove UserService injection test
    Removed test for UserService injection.

--- src/test/java/com/booking/users/UsersApplicationTests.java
+++ src/test/java/com/booking/users/UsersApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.booking.users.service.UserService;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class UsersApplicationTests {
@@ -15,11 +14,4 @@ class UsersApplicationTests {
     @Test
     void contextLoads() {
     }
-
-    @Test
-    void testUserService() {
-        // Simple test to check if UserService is injected
-        assertNotNull(userService);
-    }
-
 }
